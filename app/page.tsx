e3f3b6445723181f47518b80ddc768e153efb242
Ch4se <ch4seprojects@gmail.com>
2025-10-10 00:29:26 +0800

    feat: add logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import Link from "next/link";
 import { Button } from "./components/Button";
+import Image from "next/image";
+import logo from "../public/logo.png";
 
 export default function Landing() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1 gap-2 bg-linear-to-t from-secondary to-main relative px-6 py-8">
-        <div className="logo rounded-full w-50 h-50 bg-white absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[70%]" />
+        <div className="logo rounded-full bg-white w-[220px] h-[220px] absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[70%] flex items-center justify-center">
+          <Image
+            src={logo}
+            alt="SayTrack Logo"
+            width={180}
+            height={180}
+            className="object-contain"
+          />
+        </div>
         <div className="flex flex-col self-end">
           <p className="font-poppins text-lg text-white">Welcome to</p>
           <p className="font-poppins text-6xl font-bold text-white">SayTrack</p>
